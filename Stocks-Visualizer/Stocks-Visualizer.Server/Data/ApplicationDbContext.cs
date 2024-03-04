9c48e90e09b6175d505910c9f8c8ac9ed8667071
NagaTharun.Makkena <NagaTharun.Makkena@DESKTOP-Q45H9I7>
2024-03-04 15:14:12 +0530

    added navbar, dbconnection, model, dbcontext, repos, controller

---
+++ Stocks-Visualizer/Stocks-Visualizer.Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Oracle.EntityFrameworkCore;
+using Stocks_Visualizer.Server.Models.Domain;
+
+namespace Stocks_Visualizer.Server.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Stock> Stocks { get; set; }
+    }
+}
---
+++ Stocks-Visualizer/Stocks-Visualizer.Server/Repositories/Interface/IStockRepositorycs.cs
@@ -0,0 +1,9 @@
+﻿using Stocks_Visualizer.Server.Models.Domain;
+
+namespace Stocks_Visualizer.Server.Repositories.Interface
+{
+    public interface IStockRepositorycs
+    {
+        Task<Stock> CreateAsync(Stock stock);
+    }
+}
